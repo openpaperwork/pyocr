22b5a3e3a7806a8078cf0c7822db37f4dbcc3ed4
Jerome Flesch <jflesch@gmail.com>
2011-12-23 17:11:38 +0100

    Add setup.py: Script to install the tesseract module on the machine
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="python-tesseract",
+      version="0.1",
+      description="A Python wrapper for Google Tesseract",
+      packages=[''],
+      package_dir={ '': '' },
+      data_files=[],
+      scripts=[],
+     )
+
