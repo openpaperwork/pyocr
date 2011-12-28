f565bf93a67b27e8e69511e2a301b5d00d5af23b
Jerome Flesch <jflesch@gmail.com>
2011-12-28 22:37:46 +0100

    Add tests regarding box extraction
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- run_tests.py
+++ run_tests.py
@@ -6,6 +6,4 @@ import unittest
 from tests import tests
 
 if __name__ == '__main__':
-    all_tests = unittest.TestSuite()
-    all_tests.addTest(tests.get_txt_testsuite())
-    unittest.TextTestRunner().run(all_tests)
+    unittest.TextTestRunner().run(tests.get_all_tests())
--- tests/tests.py
+++ tests/tests.py
@@ -41,10 +41,53 @@ class TestTxt(unittest.TestCase):
         pass
 
 
-def get_txt_testsuite():
-    tests = [
+class TestBox(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def __test_txt(self, image_file, expected_box_file, lang='eng'):
+        image_file = "tests/" + image_file
+        expected_box_file = "tests/" + expected_box_file
+
+        with codecs.open(expected_box_file, 'r', encoding='utf-8') \
+                as file_descriptor:
+            expected_boxes = tesseract.read_box_file(file_descriptor)
+        expected_boxes.sort()
+
+        boxes = tesseract.image_to_string(Image.open(image_file), lang=lang,
+                                          boxes=True)
+        boxes.sort()
+
+        self.assertEqual(len(boxes), len(expected_boxes))
+
+        for i in range(0, min(len(boxes), len(expected_boxes))):
+            self.assertEqual(boxes[i], expected_boxes[i])
+
+    def test_basic(self):
+        self.__test_txt('test.png', 'test.box')
+
+    def test_european(self):
+        self.__test_txt('test-european.jpg', 'test-european.box')
+
+    def test_french(self):
+        self.__test_txt('test-french.jpg', 'test-french.box', 'fra')
+
+    def tearDown(self):
+        pass
+
+
+def get_all_tests():
+    all_tests = unittest.TestSuite()
+
+    test_names = [
         'test_basic',
         'test_european',
         'test_french',
     ]
-    return unittest.TestSuite(map(TestTxt, tests))
+    tests = unittest.TestSuite(map(TestTxt, test_names))
+    all_tests.addTest(tests)
+
+    tests = unittest.TestSuite(map(TestBox, test_names))
+    all_tests.addTest(tests)
+
+    return all_tests
