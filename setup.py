923c94dc3c6bdc10da79190e492a855401892c4b
Jerome Flesch <jflesch@gmail.com>
2013-01-26 17:56:56 +0100

    v0.1 --> v0.1-git
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="pyocr",
-      version="0.1",
+      version="0.1-git",
       description="A Python wrapper for OCR (Tesseract, Cuneiform, etc)",
       author="Jerome Flesch",
       author_email="jflesch@gmail.com",
