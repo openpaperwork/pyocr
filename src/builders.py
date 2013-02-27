facd3d32b21bbd8d4a2634a36ab5f325b61fbd4f
Jerome Flesch <jflesch@gmail.com>
2013-02-27 21:33:35 +0100

    Tesseract: hOCR: parse line as well as words
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/builders.py
+++ src/builders.py
@@ -103,6 +103,105 @@ class Box(object):
         return (position_hash ^ hash(self.content) ^ hash(self.content))
 
 
+class LineBox(object):
+    """
+    Boxes are rectangles around each individual element recognized in the
+    image. LineBox are boxes around lines. LineBox contains Box.
+    """
+
+    def __init__(self, word_boxes, position):
+        """
+        Arguments:
+            word_boxes --- a single string
+            position --- the position of the box on the image. Given as a
+                tuple of tuple:
+                ((width_pt_x, height_pt_x), (width_pt_y, height_pt_y))
+        """
+        self.word_boxes = word_boxes
+        self.position = position
+
+    def get_unicode_string(self):
+        """
+        Return the string corresponding to the box, in unicode (utf8).
+        This string can be stored in a file as-is (see write_box_file())
+        and reread using read_box_file().
+        """
+        txt = u"[\n"
+        for box in self.word_boxes:
+            txt += u"  %s\n" % box.get_unicode_string()
+        return u"%s] %d %d %d %d" % (
+            txt,
+            self.position[0][0],
+            self.position[0][1],
+            self.position[1][0],
+            self.position[1][1],
+        )
+
+    def __get_content(self):
+        txt = u""
+        for box in self.word_boxes:
+            txt += box.content + u" "
+        txt = txt.strip()
+        return txt
+
+    content = property(__get_content)
+
+    def get_xml_tag(self):
+        span_tag = xml.dom.minidom.Element("span")
+        span_tag.setAttribute("class", "ocr_line")
+        span_tag.setAttribute("title", ("bbox %d %d %d %d" % (
+                (self.position[0][0], self.position[0][1],
+                 self.position[1][0], self.position[1][1]))))
+        for box in self.word_boxes:
+            box_xml = box.get_xml_tag()
+            span_tag.appendChild(box_xml)
+        return span_tag
+
+    def __str__(self):
+        return self.get_unicode_string().encode('utf-8')
+
+    def __box_cmp(self, other):
+        """
+        Comparison function.
+        """
+        if other == None:
+            return -1
+        for cmp_result in (cmp(self.position[0][1], other.position[0][1]),
+                           cmp(self.position[1][1], other.position[1][1]),
+                           cmp(self.position[0][0], other.position[0][0]),
+                           cmp(self.position[1][0], other.position[1][0])):
+            if cmp_result != 0:
+                return cmp_result
+        return 0
+
+    def __lt__(self, other):
+        return self.__box_cmp(other) < 0
+
+    def __gt__(self, other):
+        return self.__box_cmp(other) > 0
+
+    def __eq__(self, other):
+        return self.__box_cmp(other) == 0
+
+    def __le__(self, other):
+        return self.__box_cmp(other) <= 0
+
+    def __ge__(self, other):
+        return self.__box_cmp(other) >= 0
+
+    def __ne__(self, other):
+        return self.__box_cmp(other) != 0
+
+    def __hash__(self):
+        content = self.content
+        position_hash = 0
+        position_hash += ((self.position[0][0] & 0xFF) << 0)
+        position_hash += ((self.position[0][1] & 0xFF) << 8)
+        position_hash += ((self.position[1][0] & 0xFF) << 16)
+        position_hash += ((self.position[1][1] & 0xFF) << 24)
+        return (position_hash ^ hash(content) ^ hash(content))
+
+
 class TextBuilder(object):
     """
     If passed to image_to_string(), image_to_string() will return a simple
@@ -139,6 +238,162 @@ class TextBuilder(object):
         return "Raw text"
 
 
+class _WordHTMLParser(HTMLParser):
+    """
+    Tesseract style: Tesseract provides handy but non-standard hOCR tags:
+    ocr_word
+    """
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+
+        self.__tag_types = []
+
+        self.__current_box_position = None
+        self.__current_box_text = None
+        self.boxes = []
+
+        self.__current_line_position = None
+        self.__current_line_content = []
+        self.lines = []
+
+    @staticmethod
+    def __parse_position(title):
+        title = title.split("; ")
+        title = title[-1]
+        title = title.split(" ")
+        position = ((int(title[1]), int(title[2])),
+                    (int(title[3]), int(title[4])))
+        return position
+
+    def handle_starttag(self, tag, attrs):
+        if (tag != "span"):
+            return
+        position = None
+        tag_type = None
+        for attr in attrs:
+            if attr[0] == 'class':
+                tag_type = attr[1]
+            if attr[0] == 'title':
+                position = attr[1]
+        if position is None or tag_type is None:
+            return
+        self.__tag_types.append(tag_type)
+        if tag_type == 'ocr_word':
+            self.__current_box_position = self.__parse_position(position)
+            self.__current_box_text = u""
+        elif tag_type == 'ocr_line':
+            self.__current_line_position = self.__parse_position(position)
+            self.__current_line_content = []
+
+    def handle_data(self, data):
+        if self.__current_box_text == None:
+            return
+        self.__current_box_text += unicode(data)
+
+    def handle_endtag(self, tag):
+        if tag != 'span':
+            return
+        tag_type = self.__tag_types.pop()
+        if tag_type == 'ocr_word':
+            if (self.__current_box_text == None):
+                return
+            box_position = self.__current_box_position
+            box = Box(self.__current_box_text, box_position)
+            self.boxes.append(box)
+            self.__current_line_content.append(box)
+            self.__current_box_text = None
+            return
+        if tag_type == 'ocr_line':
+            line = LineBox(self.__current_line_content,
+                           self.__current_line_position)
+            self.lines.append(line)
+            self.__current_line_content = []
+            return
+
+    @staticmethod
+    def __str__():
+        return "WordHTMLParser"
+
+
+class _LineHTMLParser(HTMLParser):
+    """
+    Cuneiform style: Cuneiform provides the OCR line by line, and for each
+    line, the position of all its characters.
+    Spaces have "-1 -1 -1 -1" for position".
+    """
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.boxes = []
+        self.__line_text = None
+        self.__char_positions = None
+
+    def handle_starttag(self, tag, attrs):
+        TAG_TYPE_CONTENT = 0
+        TAG_TYPE_POSITIONS = 1
+
+        if (tag != "span"):
+            return
+        tag_type = -1
+        for attr in attrs:
+            if attr[0] == 'class':
+                if attr[1] == 'ocr_line':
+                    tag_type = TAG_TYPE_CONTENT
+                elif attr[1] == 'ocr_cinfo':
+                    tag_type = TAG_TYPE_POSITIONS
+
+        if tag_type == TAG_TYPE_CONTENT:
+            self.__line_text = u""
+            self.__char_positions = []
+            return
+        elif tag_type == TAG_TYPE_POSITIONS:
+            for attr in attrs:
+                if attr[0] == 'title':
+                    self.__char_positions = attr[1].split(" ")
+            # strip x_bboxes
+            self.__char_positions = self.__char_positions[1:]
+            if self.__char_positions[-1] == "":
+                self.__char_positions[:-1]
+            try:
+                while True:
+                    self.__char_positions.remove("-1")
+            except ValueError:
+                pass
+
+    def handle_data(self, data):
+        if self.__line_text == None:
+            return
+        self.__line_text += data
+
+    def handle_endtag(self, tag):
+        if self.__line_text == None or self.__char_positions == []:
+            return
+        words = self.__line_text.split(" ")
+        for word in words:
+            if word == "":
+                continue
+            positions = self.__char_positions[0:4 * len(word)]
+            self.__char_positions = self.__char_positions[4 * len(word):]
+
+            left_pos = min([int(positions[x])
+                            for x in range(0, 4 * len(word), 4)])
+            top_pos = min([int(positions[x])
+                           for x in range(1, 4 * len(word), 4)])
+            right_pos = max([int(positions[x])
+                             for x in range(2, 4 * len(word), 4)])
+            bottom_pos = max([int(positions[x])
+                              for x in range(3, 4 * len(word), 4)])
+
+            box_pos = ((left_pos, top_pos), (right_pos, bottom_pos))
+            box = Box(word, box_pos)
+            self.boxes.append(box)
+        self.__line_text = None
+
+    @staticmethod
+    def __str__():
+        return "LineHTMLParser"
+
+
 class WordBoxBuilder(object):
     """
     If passed to image_to_string(), image_to_string() will return an array of
@@ -152,135 +407,6 @@ class WordBoxBuilder(object):
     def __init__(self):
         pass
 
-    class WordHTMLParser(HTMLParser):
-        """
-        Tesseract style: Tesseract provides handy but non-standard hOCR tags:
-        ocr_word
-        """
-        def __init__(self):
-            HTMLParser.__init__(self)
-            self.__current_box_position = None
-            self.__current_box_text = None
-            self.boxes = []
-
-        @staticmethod
-        def __parse_position(title):
-            title = title.split("; ")
-            title = title[-1]
-            title = title.split(" ")
-            position = ((int(title[1]), int(title[2])),
-                        (int(title[3]), int(title[4])))
-            return position
-
-        def handle_starttag(self, tag, attrs):
-            if (tag != "span"):
-                return
-            position = None
-            for attr in attrs:
-                if (attr[0] == 'class'
-                    and (attr[1] != 'ocr_word')):
-                    return
-                if attr[0] == 'title':
-                    position = attr[1]
-            if position == None:
-                return
-            self.__current_box_position = self.__parse_position(position)
-            self.__current_box_text = u""
-
-        def handle_data(self, data):
-            if self.__current_box_text == None:
-                return
-            self.__current_box_text += data
-
-        def handle_endtag(self, tag):
-            if (self.__current_box_text == None):
-                return
-            box_position = self.__current_box_position
-            box = Box(self.__current_box_text, box_position)
-            self.boxes.append(box)
-            self.__current_box_text = None
-
-        @staticmethod
-        def __str__():
-            return "WordHTMLParser"
-
-    class LineHTMLParser(HTMLParser):
-        """
-        Cuneiform style: Cuneiform provides the OCR line by line, and for each
-        line, the position of all its characters.
-        Spaces have "-1 -1 -1 -1" for position".
-        """
-        def __init__(self):
-            HTMLParser.__init__(self)
-            self.boxes = []
-            self.__line_text = None
-            self.__char_positions = None
-
-        def handle_starttag(self, tag, attrs):
-            TAG_TYPE_CONTENT = 0
-            TAG_TYPE_POSITIONS = 1
-
-            if (tag != "span"):
-                return
-            tag_type = -1
-            for attr in attrs:
-                if attr[0] == 'class':
-                    if attr[1] == 'ocr_line':
-                        tag_type = TAG_TYPE_CONTENT
-                    elif attr[1] == 'ocr_cinfo':
-                        tag_type = TAG_TYPE_POSITIONS
-
-            if tag_type == TAG_TYPE_CONTENT:
-                self.__line_text = u""
-                self.__char_positions = []
-                return
-            elif tag_type == TAG_TYPE_POSITIONS:
-                for attr in attrs:
-                    if attr[0] == 'title':
-                        self.__char_positions = attr[1].split(" ")
-                # strip x_bboxes
-                self.__char_positions = self.__char_positions[1:]
-                if self.__char_positions[-1] == "":
-                    self.__char_positions[:-1]
-                try:
-                    while True:
-                        self.__char_positions.remove("-1")
-                except ValueError:
-                    pass
-
-        def handle_data(self, data):
-            if self.__line_text == None:
-                return
-            self.__line_text += data
-
-        def handle_endtag(self, tag):
-            if self.__line_text == None or self.__char_positions == []:
-                return
-            words = self.__line_text.split(" ")
-            for word in words:
-                if word == "":
-                    continue
-                positions = self.__char_positions[0:4 * len(word)]
-                self.__char_positions = self.__char_positions[4 * len(word):]
-
-                left_pos = min([int(positions[x])
-                                for x in range(0, 4 * len(word), 4)])
-                top_pos = min([int(positions[x])
-                               for x in range(1, 4 * len(word), 4)])
-                right_pos = max([int(positions[x])
-                                 for x in range(2, 4 * len(word), 4)])
-                bottom_pos = max([int(positions[x])
-                                  for x in range(3, 4 * len(word), 4)])
-
-                box_pos = ((left_pos, top_pos), (right_pos, bottom_pos))
-                box = Box(word, box_pos)
-                self.boxes.append(box)
-            self.__line_text = None
-
-        @staticmethod
-        def __str__():
-            return "LineHTMLParser"
-
     def read_file(self, file_descriptor):
         """
         Extract of set of Box from the lines of 'file_descriptor'
@@ -288,7 +414,7 @@ class WordBoxBuilder(object):
         Return:
             An array of Box.
         """
-        parsers = [self.WordHTMLParser(), self.LineHTMLParser()]
+        parsers = [_WordHTMLParser(), _LineHTMLParser()]
         html_str = file_descriptor.read()
 
         for p in parsers:
@@ -316,3 +442,57 @@ class WordBoxBuilder(object):
     @staticmethod
     def __str__():
         return "Word boxes"
+
+
+class LineBoxBuilder(object):
+    """
+    If passed to image_to_string(), image_to_string() will return an array of
+    LineBox. Each box contains a word recognized in the image.
+    """
+
+    file_extension = "html"
+    tesseract_configs = ['hocr']
+    cuneiform_args = ["-f", "hocr"]
+
+    def __init__(self):
+        pass
+
+    def read_file(self, file_descriptor):
+        """
+        Extract of set of Box from the lines of 'file_descriptor'
+
+        Return:
+            An array of LineBox.
+        """
+        parsers = [
+            (_WordHTMLParser(), lambda parser: parser.lines),
+            (_LineHTMLParser(), lambda parser: [LineBox([box], box.position)
+                                                for box in parser.boxes]),
+        ]
+        html_str = file_descriptor.read()
+
+        for (parser, convertion) in parsers:
+            parser.feed(html_str)
+            if len(parser.boxes) > 0:
+                return convertion(parser)
+        return []
+
+    @staticmethod
+    def write_file(file_descriptor, boxes):
+        """
+        Write boxes in a box file. Output is a *very* *simplified* version
+        of hOCR.
+
+        Warning:
+            The file_descriptor must support UTF-8 ! (see module 'codecs')
+        """
+        file_descriptor.write(u"<body>\n")
+        for box in boxes:
+            xml_str = box.get_xml_tag().toxml()
+            xml_utf = xml_str.decode('utf-8')
+            file_descriptor.write(xml_utf + u"\n")
+        file_descriptor.write(u"</body>\n")
+
+    @staticmethod
+    def __str__():
+        return "Line boxes"
--- tests/tests_tesseract.py
+++ tests/tests_tesseract.py
@@ -199,6 +199,68 @@ class TestWordBox(unittest.TestCase):
         pass
 
 
+class TestLineBox(unittest.TestCase):
+    """
+    These tests make sure that Tesseract box handling works fine.
+    """
+    def setUp(self):
+        self.builder = builders.LineBoxBuilder()
+
+    def __test_txt(self, image_file, expected_box_file, lang='eng'):
+        image_file = "tests/data/" + image_file
+        expected_box_file = "tests/tesseract/" + expected_box_file
+
+        boxes = tesseract.image_to_string(Image.open(image_file), lang=lang,
+                                          builder=self.builder)
+        boxes.sort()
+
+        with codecs.open(expected_box_file, 'r', encoding='utf-8') \
+                as file_descriptor:
+            expected_boxes = self.builder.read_file(file_descriptor)
+        expected_boxes.sort()
+
+        self.assertEqual(len(boxes), len(expected_boxes))
+
+        for i in range(0, min(len(boxes), len(expected_boxes))):
+            for j in range(0, len(boxes[i].word_boxes)):
+                self.assertEqual(type(boxes[i].word_boxes[j]),
+                                 type(expected_boxes[i].word_boxes[j]))
+            self.assertEqual(boxes[i], expected_boxes[i])
+
+    def test_basic(self):
+        self.__test_txt('test.png', 'test.lines')
+
+    def test_european(self):
+        self.__test_txt('test-european.jpg', 'test-european.lines')
+
+    def test_french(self):
+        self.__test_txt('test-french.jpg', 'test-french.lines', 'fra')
+
+    def test_write_read(self):
+        original_boxes = tesseract.image_to_string(
+            Image.open("tests/data/test.png"), builder=self.builder)
+        self.assertTrue(len(original_boxes) > 0)
+
+        (file_descriptor, tmp_path) = tempfile.mkstemp()
+        try:
+            # we must open the file with codecs.open() for utf-8 support
+            os.close(file_descriptor)
+
+            with codecs.open(tmp_path, 'w', encoding='utf-8') as file_descriptor:
+                self.builder.write_file(file_descriptor, original_boxes)
+
+            with codecs.open(tmp_path, 'r', encoding='utf-8') as file_descriptor:
+                new_boxes = self.builder.read_file(file_descriptor)
+
+            self.assertEqual(len(new_boxes), len(original_boxes))
+            for i in range(0, len(original_boxes)):
+                self.assertEqual(new_boxes[i], original_boxes[i])
+        finally:
+            os.remove(tmp_path)
+
+    def tearDown(self):
+        pass
+
 def get_all_tests():
     all_tests = unittest.TestSuite()
 
@@ -226,14 +288,9 @@ def get_all_tests():
     ]
     tests = unittest.TestSuite(map(TestCharBox, test_names))
     all_tests.addTest(tests)
-
-    test_names = [
-        'test_basic',
-        'test_european',
-        'test_french',
-        'test_write_read',
-    ]
     tests = unittest.TestSuite(map(TestWordBox, test_names))
+    all_tests.addTest(tests)
+    tests = unittest.TestSuite(map(TestLineBox, test_names))
     all_tests.addTest(tests)
 
     return all_tests
