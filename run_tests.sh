c2e45b110437f2ebe891dde430fa3b38d6906198
Jerome Flesch <jflesch@openpaper.work>
2018-01-25 11:56:04 +0100

    add test script to test with Python2 & 3
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ret=0
+
+
+for pyver in python3 python2 ; do
+    echo "# Testing with ${pyver}"
+    rm -rf venv-${pyver}
+    virtualenv -p ${pyver} --system-site-packages venv-${pyver}
+    . venv-${pyver}/bin/activate
+    if ! ${pyver} ./setup.py install ; then
+        echo "Install failed"
+        exit 1
+    fi
+
+    if ! ${pyver} ./run_tests.py ; then
+        echo "Tests failed"
+        ret=1
+    fi
+done
+
+exit ${ret}
