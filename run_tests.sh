4740c836fd329059f3c476aa3f7de8dfc4622214
Jerome Flesch <jflesch@openpaper.work>
2018-01-25 16:42:13 +0100

    When testing, do not use system-site-packages
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ ret=0
 for pyver in python3 python2 ; do
     echo "# Testing with ${pyver}"
     rm -rf venv-${pyver}
-    virtualenv -p ${pyver} --system-site-packages venv-${pyver}
+    virtualenv -p ${pyver} venv-${pyver}
     . venv-${pyver}/bin/activate
     if ! ${pyver} ./setup.py install ; then
         echo "Install failed"
